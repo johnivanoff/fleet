63cec9eb55665649b7b61eb06ecb645db2fd7c20
John Ivanoff <john.ivanoff@gmail.com>
2011-12-14 09:41:40 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fleet::Application.config.secret_token = 'decd88c8bc7765af02e29f628989722195d5978472de904733435593b4b41c718962f2640aa18311091b9590bb2dfd4d8a721f87f93d06a54d2bfb30512c1c28'
