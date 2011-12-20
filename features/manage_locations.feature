252b67c687f61a97fe837d799c430d3b1299b40d
John Ivanoff <john.ivanoff@gmail.com>
2011-12-20 14:06:42 -0600

    new test for make a location

---
+++ features/manage_locations.feature
@@ -0,0 +1,10 @@
+Feature: Manage locations
+  In order to manage locations
+  As a user
+  I want to create and edit my locations.
+  
+  Scenario: Register new role
+	Given I am on the new location page
+	And I fill in "Name" with "location 1"
+	And press "Create"
+	Then I should see "location 1"
---
+++ features/step_definitions/location_steps.rb
@@ -0,0 +1,15 @@
+Given /^(?:|I )am on (.+)$/ do |page_name|
+  visit(page_name)
+end
+
+Given /^I fill in "([^"]*)" with "([^"]*)"$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^press "([^"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see "([^"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
