ae57fd62c91c55df81cf826f3b076a30bef439cb
John Ivanoff <john.ivanoff@gmail.com>
2012-01-11 10:32:17 -0600

    i18n set u p in location helper

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -1,30 +1,25 @@
 module LocationsHelper
   
   def edit_link(c)
-    link_to "edit_html", edit_location_path(c)
+    link_to t('.edit_html'), edit_location_path(c)
   end
   
   def show_link(c)
-    link_to t('.show_thml'), location_path(c)
+    link_to t('.show_html'), location_path(c)
   end
   
   def destroy_link(c)
     link_to t('.destroy_html'), c, :confirm => 'Are you sure?', :method => :delete
   end
  
-  def completed?(location)
-#    RedCloth.new(check_box_tag("location["+location.id.to_s+"][completed]", "1", location["completed"]==true, "onclick" => "document.location.href='#{complete_location_path(location)}'") + 
-#    " " + 
-#    location.name).to_html
-#    check_box_tag("location["+location.id.to_s+"][completed]", "1", location["completed"]==true, "onclick" => "document.location.href='#{completed_location_path(location)}'") + 
-#   " " + 
+  def location_name?(location)
     location.name
   end
 
   def location_tree location
     content_tag :ul do
       location.children.map do |c|
-        content_tag(:li, completed?(c).html_safe + " " + edit_link(c) + " " + show_link(c) + " " + destroy_link(c)) +
+        content_tag(:li, location_name?(c).html_safe + " " + edit_link(c) + " " + show_link(c) + " " + destroy_link(c)) +
         (location_tree c unless c.children.empty?).to_s
       end.join.html_safe
     end
